ab099fa094ad1e9782ce21adde42975b2a0f099f
Khoa Do <khoado.dk@gmail.com>
2019-07-25 18:29:57 -0400

    add animation

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`} style={{ backgroundImage: `url(${imageUrl})`}}>
-        <div className='content'>
-          <div className='title'>{title}</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
+    <div className={`${size} menu-item`}>
+      <div className='background-image' style={{ backgroundImage: `url(${imageUrl})`}}/>
+      <div className='content'>
+        <div className='title'>{title.toUpperCase()}</div>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
 )
 export default MenuItem;
